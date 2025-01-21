366c8609491b51e4b5b20ed019c9f0ad9b971412
ruben-g922 <rgarcia25@madisoncollege.edu>
2025-01-21 04:28:55 -0600

    generateArray function

--- generate.js
+++ generate.js
@@ -14,25 +14,28 @@ const generateArray = () => {
     length = document.querySelector("#length");
     multi = document.querySelector("#maxnum");
     let neg = document.querySelector("#negatives").checked;
-    console.log(neg);
+    // TODO handle larger numbers
     if ((isNaN(length.value) || isNaN(multi.value)) || (length.value === "" || multi.value === "" )) {
-        console.log("Not a number!");
-        err.innerHTML = "Could Not Generate! Are inputs numerical?";
+        err.innerHTML = "Could not generate! Are inputs numerical?";
     } else {
         length = parseInt(length.value);
         multi = parseInt(multi.value);
-        err.innerHTML = "";
-        let output = "";
-        let addNeg = 0;
-        if (neg) addNeg = 0.5
-        output += "[";
-        for (let i = 0; i < length; i++) {
-            let temp = Math.floor((Math.random()-neg)*multi);
-            output += temp;
-            if (i != length-1) output+= ",";
+        textarea.innerHTML = "";
+        let temp = "";
+        if (((length > 2000000  || multi > 2147483647 )) || (length < 0 || multi < 0)) {
+            err.innerHTML = "Could not generate! Are inputs non-negative and below the maximum?";
+        } else {
+            err.innerHTML = "";
+            let addNeg = 0;
+            if (neg) addNeg = 0.5
+            temp += "[";
+            for (let i = 0; i < length; i++) {
+                temp += Math.floor((Math.random()-addNeg)*multi);
+                if (i != length-1) temp += ",";
+            }
+            temp += "]";
+            textarea.innerHTML = temp;
         }
-        output += "]";
-        textarea.innerHTML = output;
     }
     }
     
