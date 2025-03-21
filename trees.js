35c26d0cd4c259a1888b07e7918f97ae9dcd340a
ruben-g922 <rgarcia25@madisoncollege.edu>
2025-03-21 03:10:34 -0500

    Reworked tree visualization, added BT class implementation, method to construct BT from array

--- trees.js
+++ trees.js
@@ -21,11 +21,13 @@ const init = () => {
     graphics = document.querySelector("#visualizer");
     
     // TODO update this
-    let temp = document.querySelector("#tempbtn");
+    // let temp = document.querySelector("#tempbtn");
     
     btn.addEventListener("click", clicked);
 }
 
+
+// TODO refactor into promise
 const clicked = () => {
     let depth = maxdepth.value;
     let lowDepth = mindepth.value;
@@ -71,15 +73,31 @@ const clicked = () => {
 
     let width = window.innerWidth || document.documentElement.clientWidth || document.body.clientWidth;
     graphics.innerHTML = "";
-    if (width > 700 && res[0] != 0) {
-        generateImage(0, 1, -1, 0, res);
+    if (width > 700 && res[0] != null) {
+        let lines = document.createElementNS('http://www.w3.org/2000/svg', 'g');
+
+        lines.setAttributeNS(null, "id", "lines");
+        graphics.appendChild(lines);
+        let head = constructTree(res);
+    
+        if (head != null) generateImage(0, 1, -1, -10, head);
     }
 }
 
 
-
-let generateImage = (depth, pos, lastx, lasty, res, i) => {
+/**
+ * Generages an SVG binary tree
+ * @param {Number} depth 
+ * @param {Number} pos 
+ * @param {Number} lastx 
+ * @param {Number} lasty 
+ * @param {Node} node 
+ * @returns 
+ */
+let generateImage = (depth, pos, lastx, lasty, node) => {
+    if (node === null || depth > 3) return;
     let w = Math.pow(2, depth)+1;
+    let h = lasty+20;
     w = (100/w)*pos;
 
     let bg = document.createElementNS('http://www.w3.org/2000/svg', 'circle');
@@ -88,31 +106,94 @@ let generateImage = (depth, pos, lastx, lasty, res, i) => {
     let text = document.createElementNS('http://www.w3.org/2000/svg', 'text');
 
     bg.setAttributeNS(null, "cx", w+"%");
-    bg.setAttributeNS(null, "cy", (lasty+10)+"%");
+    bg.setAttributeNS(null, "cy", (h)+"%");
     bg.setAttributeNS(null, "r", "50");
-    bg.setAttributeNS(null, "fill", "#212729");
+    bg.setAttributeNS(null, "fill", "#3f4447");
 
     circ.setAttributeNS(null, "cx", w+"%");
-    circ.setAttributeNS(null, "cy", (lasty+10)+"%");
+    circ.setAttributeNS(null, "cy", (h)+"%");
     circ.setAttributeNS(null, "r", "50");
     circ.setAttributeNS(null, "stroke", "white");
     circ.setAttributeNS(null, "stroke-width", "5");
     circ.setAttributeNS(null, "fill", "none");
 
     outer.setAttributeNS(null, "cx", w+"%");
-    outer.setAttributeNS(null, "cy", (lasty+10)+"%");
+    outer.setAttributeNS(null, "cy", (h)+"%");
     outer.setAttributeNS(null, "r", "63");
-    outer.setAttributeNS(null, "stroke", "#212729");
+    outer.setAttributeNS(null, "stroke", "#3f4447");
     outer.setAttributeNS(null, "stroke-width", "23");
     outer.setAttributeNS(null, "fill", "none");
 
     text.setAttributeNS(null, "x", w+"%");
-    text.setAttributeNS(null, "y", (lasty+10)+"%");
-    text.innerHTML = res[i];
+    text.setAttributeNS(null, "y", (h)+"%");
+    text.innerHTML = node.val;
 
     graphics.appendChild(bg);
     graphics.appendChild(text);
     graphics.appendChild(outer);
     graphics.appendChild(circ);
+
+    if (depth != 0) {
+        let line = document.createElementNS('http://www.w3.org/2000/svg', 'line');
+        line.setAttributeNS(null, "stroke", "white");
+        line.setAttributeNS(null, "stroke-width", "5");
+        line.setAttributeNS(null, "x1",lastx+"%");
+        line.setAttributeNS(null, "y1",(lasty)+"%")
+        line.setAttributeNS(null, "x2",w+"%");
+        line.setAttributeNS(null, "y2",(h)+"%");
+        document.querySelector("#lines").appendChild(line);
+    }
+
+    generateImage(depth+1, (pos*2)-1, w, lasty+20, node.left);
+    generateImage(depth+1, pos*2, w, lasty+20, node.right);
+}
+
+
+
+/**
+ * Constructs a tree based off the given array
+ * @param {Array} list 
+ * @returns Head of constructed tree
+ */
+let constructTree = (list) => {
+
+    // TODO make this work with not just "proper" arrays
+    if (list.length%2 == 0) return null;
+
+
+    let queue = [];
+    let head = new Node();
+    head.val = list[0];
+
+    queue.push(head);
+    for (let i = 1; i < list.length; i+=2) {
+        let temp = queue.shift();
+        if (list[i] !== null && list[i] !== "null") {
+            let left = new Node();
+            left.val = list[i];
+            temp.left = left;
+            queue.push(left);
+        }
+        if (list[i+1] !== null && list[i+1] !== "null") {
+            let right = new Node();
+            right.val = list[i+1];
+            temp.right = right;
+            queue.push(right);
+        }
+    }
+
+    return head;
+}
+
+
+/**
+ * Tree Node util class
+ */
+class Node {
+    constructor() {
+        this.left = null;
+        this.right = null;
+        this.val = 0;
+    }
 
 }
