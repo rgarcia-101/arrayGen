c45b24b8121e9f11891e7c17d9897cd141d77836
rgarcia-101 <ruben@garcia101.com>
2025-04-07 21:20:03 -0500

    Updated navbar to include list generator

--- startup.js
+++ startup.js
@@ -14,6 +14,9 @@ const headerGen = () => {
     let li3 = document.createElement("li"); 
     let matrices = document.createElement("a");
 
+    let li4 = document.createElement("li"); 
+    let lists = document.createElement("a");
+
     arrays.href = "arrays.html";
     arrays.innerHTML = "Array Generator";
 
@@ -23,11 +26,16 @@ const headerGen = () => {
     trees.href = "trees.html";
     trees.innerHTML = "Binary Trees";
 
+    lists.href = "lists.html"
+    lists.innerHTML = "Custom Array"
+
     li1.appendChild(arrays);
     li2.appendChild(matrices);
     li3.appendChild(trees);
+    li4.appendChild(lists);
 
     list.appendChild(li1);
+    list.appendChild(li4);
     list.appendChild(li2);
     list.appendChild(li3);
 }
