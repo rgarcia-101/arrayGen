72e61d7de0cfb2dd407b4a563ac5b9519a02a775
rgarcia-101 <ruben@garcia101.com>
2025-04-07 21:29:13 -0500

    Added preset inputs

--- lists.js
+++ lists.js
@@ -94,3 +94,19 @@ async function clicked() {
         btn.disabled = false;
     }
 }
+
+
+
+const presets = (type) => {
+    switch (type) {
+        case "lowercase":
+            inputtext.value = "a,b,c,d,e,f,g,h,i,j,k,l,m,n,o,p,q,r,s,t,u,v,w,x,y,z";
+            break;
+        case "uppercase":
+            inputtext.value = "A,B,C,D,E,F,G,H,I,J,K,L,M,N,O,P,Q,R,S,T,U,V,W,X,Y,Z";
+            break;
+        case "allLetters":
+            inputtext.value = "a,b,c,d,e,f,g,h,i,j,k,l,m,n,o,p,q,r,s,t,u,v,w,x,y,z,A,B,C,D,E,F,G,H,I,J,K,L,M,N,O,P,Q,R,S,T,U,V,W,X,Y,Z";
+            break;
+    }
+}
