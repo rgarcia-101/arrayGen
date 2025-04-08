8b35c162f8ae37da9342aaef180f4b8190a3227d
rgarcia-101 <ruben@garcia101.com>
2025-04-07 21:19:09 -0500

    Lists generator JS

---
+++ lists.js
@@ -0,0 +1,96 @@
+const MAX = 2000000;
+const MAX_INT = 2147483647;
+let textarea;
+let err;
+let btn;
+let process;
+let lengthbox;
+let inputtext
+
+
+
+/**
+ * Runs on page load, loads document objects into vars
+ */
+const init = () => {
+    btn = document.querySelector("#generatebtn");
+    textarea = document.querySelector("#outputBox");
+    inputtext = document.querySelector("#inBox");
+    process = document.querySelector("#processtext");
+    err = document.querySelector("#errortext");
+    lengthbox = document.querySelector("#length");
+
+    lengthbox.placeholder = "Max " + MAX;
+
+
+    btn.addEventListener("click", clicked);
+}
+
+
+
+/**
+ * 
+ */
+async function clicked() {
+    btn.disabled = true;
+    err.innerHTML = "";
+    process.innerHTML = "Generating...";
+
+    let promise = new Promise((resolve, reject) => {
+        process.innerHTML = "Generating...";
+
+        setTimeout(() => {
+            let length = lengthbox.value;
+            let input = inputtext.value;
+            // TODO handle larger numbers
+            if (isNaN(length) || (input === "" || length === "")) {
+                reject(Error("1"));
+                return;
+            } else {
+                // TODO organize, break into functions
+                length = parseInt(length);
+                input = input.split(",")
+                if (((input.length > MAX || length > MAX)) || (length < 0)) {
+                    reject(Error("2"));
+                    return;
+                } else {
+                    let inputSize = input.length;
+                    let temp = [];
+                    for (let i = 0; i < length; i++) {
+                        temp.push(input[Math.floor(Math.random()*inputSize)].trim());
+                    }
+
+
+                    process.innerHTML = "";
+                    output = "[" + temp.toString() + "]";
+                }
+                textarea.value = output;
+                resolve("0");
+            }
+        }, 200);
+    }).catch(error => {
+        process.innerHTML = "";
+        switch (error.message) {
+            case "1":
+                console.log("nan/empty");
+                err.innerHTML = "Could not generate! Are inputs filled out and numerical?"
+                break;
+            case "2":
+                console.log("impropernum");
+                err.innerHTML = "Could not generate! Are inputs in the valid range?";
+                break;
+            default:
+                console.log("other");
+                err.innerHTML = "Could not generate! Something went wrong."
+                break;
+        }
+        btn.disabled = false;
+    });
+
+    let result = await promise;
+
+    if (result === "0") {
+        console.log("success");
+        btn.disabled = false;
+    }
+}
