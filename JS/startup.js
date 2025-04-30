efc5eabf30c7a23bb296dd0ca49004963e937f9a
rgarcia-101 <ruben@garcia101.com>
2025-04-30 17:06:52 -0500

    Added and reformatted links

--- JS/startup.js
+++ JS/startup.js
@@ -1,21 +1,22 @@
 
 
 const headerGen = () => {
-    let div = document.createElement("div");
+    // let div = document.createElement("div");
     let list = document.querySelector("#navbar-nav")
 
     // List items
-    let li1 = document.createElement("li"); 
+    let li1 = document.createElement("li");
+    let li2 = document.createElement("li");
+    let li3 = document.createElement("li");
+    let li4 = document.createElement("li");
+    let li5 = document.createElement("li");
+
+
     let trees = document.createElement("a");
-
-    let li2 = document.createElement("li"); 
     let arrays = document.createElement("a");
-
-    let li3 = document.createElement("li"); 
     let matrices = document.createElement("a");
-
-    let li4 = document.createElement("li"); 
     let lists = document.createElement("a");
+    let cmatrix = document.createElement("a");
 
     arrays.href = "arrays.html";
     arrays.innerHTML = "Array Generator";
@@ -29,13 +30,20 @@ const headerGen = () => {
     lists.href = "lists.html"
     lists.innerHTML = "Custom Array"
 
+    cmatrix.href = "cmatrix.html";
+    cmatrix.innerHTML = "Custom Matrix";
+
+
+
     li1.appendChild(arrays);
     li2.appendChild(matrices);
-    li3.appendChild(trees);
-    li4.appendChild(lists);
+    li3.appendChild(lists);
+    li4.appendChild(cmatrix);
+    li5.appendChild(trees);
 
     list.appendChild(li1);
-    list.appendChild(li4);
     list.appendChild(li2);
     list.appendChild(li3);
+    list.appendChild(li4);
+    list.appendChild(li5);
 }
